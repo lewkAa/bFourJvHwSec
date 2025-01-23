dd6c08604919541d442fff5cb10411474c0447e3
lewkAa <tihonovtan@gmail.com>
2025-01-23 15:46:54 +0300

    add Main.java and BmiService.java

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public double calculate(double a, double b) {
+        double result =  (a / (b * b));
+        return (int) result;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 98;  // вес в кг
+        double height = 1.87; // рост в метрах
+
+        double calc = service.calculate(weight, height);
+        int index = (int)calc;
+
+        System.out.println("\nВаш индекс массы тела составляет: ");
+        System.out.println(index);
+
+        System.out.println("\nВаш индекс массы тела составляет: ");
+        System.out.println((int)service.calculate(100, 1.66));
+
+        System.out.println("\nВаш индекс массы тела составляет: ");
+        System.out.println((int)service.calculate(70, 1.88));
+
+        System.out.println("\nВаш индекс массы тела составляет: ");
+        System.out.println((int)service.calculate(66, 1.75));
+    }
+}
